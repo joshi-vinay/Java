65b9b20b3009cdea13d8e088775837d40da0b195
Vinay Joshi <joshivinay085@gmail.com>
2024-01-21 13:16:17 +0530

    Taking input in java

---
+++ src/Inputs.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+        System.out.println(input.nextLine());
+    }
+}
