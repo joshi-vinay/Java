b365387ffe52b35f692d17a8ba0b6500ccbe1c64
Vinay Joshi <joshivinay085@gmail.com>
2024-01-24 19:51:40 +0530

    Recover deleted files

--- src/t1_Basics/Inputs.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Inputs {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-//        System.out.println(input.nextInt());
-        System.out.println(input.nextLine());
-    }
-}
