89edc1f87e02af9dc60e611e013105fbd69a39d9
Vinay Joshi <joshivinay085@gmail.com>
2024-01-24 19:44:37 +0530

    Basics

---
+++ src/t1_Basics/LoopBasics.java
@@ -0,0 +1,21 @@
+public class LoopBasics {
+    public static void main(String[] args) {
+//        if(true){
+//            System.out.println("Hello World");
+//        }
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+        for(int count = 1; count != 5; count++){
+            System.out.println(count);
+        }
+
+//        When to use while loop and when to use for loop--> The simple answer is that when you don't know
+//            how many times the loop is going to run, use while loop and when you know how many times the
+//                loop is going to run, use for loop.
+    }
+}
