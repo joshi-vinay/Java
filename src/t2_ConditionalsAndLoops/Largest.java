750ab4decc02b7e72d0b879373b585c8a760e777
Vinay Joshi <joshivinay085@gmail.com>
2024-01-29 18:40:31 +0530

    Largest number check

--- src/t2_ConditionalsAndLoops/Largest.java
+++ src/t2_ConditionalsAndLoops/Largest.java
@@ -14,27 +14,27 @@ public class Largest {
         int num3 = in.nextInt();
 
         //  Method 1
-        if(num1 > num2 && num1 > num3){
-            System.out.println(num1 + " is greater than " + num2 + " and " + num3 );
-        } else if (num2 > num1 && num2 > num3) {
-            System.out.println(num2 + " is greater than " + num3 + " and " + num1);
-        }
-        else{
-            System.out.println(num3 + " is greater than " + num1 + " and " + num2);
-        }
+//        if(num1 > num2 && num1 > num3){
+//            System.out.println(num1 + " is greater than " + num2 + " and " + num3 );
+//        } else if (num2 > num1 && num2 > num3) {
+//            System.out.println(num2 + " is greater than " + num3 + " and " + num1);
+//        }
+//        else{
+//            System.out.println(num3 + " is greater than " + num1 + " and " + num2);
+//        }
 
         //  Method 2
-        int max = num1;
-        if(num2 > max){
-            max = num2;
-        }
-        if(num3 > max){
-            max = num3;
-        }
-        System.out.println(max + " is the greatest among the three");
+//        int max = num1;
+//        if(num2 > max){
+//            max = num2;
+//        }
+//        if(num3 > max){
+//            max = num3;
+//        }
+//        System.out.println(max + " is the greatest among the three");
 
         //  Method 3
-//        int max = Math.max(num3,Math.max(num1,num2)) ;
+        int max = Math.max(num3,Math.max(num1,num2)) ;
         System.out.println(max + " is maximum.");
 
     }
