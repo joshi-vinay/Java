3d15857a55288bfc3cec89f027d697c41a17298f
Vinay Joshi <joshivinay085@gmail.com>
2024-01-30 21:58:07 +0530

    nth Fibonacii number

---
+++ src/t2_ConditionalsAndLoops/Fibonacci.java
@@ -0,0 +1,23 @@
+package t2_ConditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+
+        System.out.println(b);
+    }
+}
