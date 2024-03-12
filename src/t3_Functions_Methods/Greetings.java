7f5d6ef90f57854f13dd3f2be438a3121789b85b
Vinay Joshi <joshivinay085@gmail.com>
2024-03-12 08:39:02 +0530

    return String

---
+++ src/t3_Functions_Methods/Greetings.java
@@ -0,0 +1,12 @@
+package t3_Functions_Methods;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Greeting();
+        Greeting();
+        Greeting();
+    }
+    static void Greeting(){
+        System.out.println("Hello Java! Vinay this side..");
+    }
+}
