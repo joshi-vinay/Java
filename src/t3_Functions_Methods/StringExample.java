1df73b3a3205da6cf34f78543580dbd070955eb8
Vinay Joshi <joshivinay085@gmail.com>
2024-03-12 08:39:33 +0530

    String Example

---
+++ src/t3_Functions_Methods/StringExample.java
@@ -0,0 +1,14 @@
+package t3_Functions_Methods;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+
+    }
+    static String greet(){
+        String greeting = "How are you";
+        return greeting;
+    }
+
+}
