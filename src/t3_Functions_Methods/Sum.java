7713c4442336a011f2821cb73c6d091eed11682d
Vinay Joshi <joshivinay085@gmail.com>
2024-03-12 08:37:58 +0530

    Sum

---
+++ src/t3_Functions_Methods/Sum.java
@@ -0,0 +1,38 @@
+package t3_Functions_Methods;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.print("Enter value of num1 : ");
+        num1 = in.nextInt();
+        System.out.print("Enter value of num2 : ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+//        System.out.print("The sum of "+num1+" and "+num2+" is : "+sum);
+        return  sum;
+    }
+
+//    static void sum(){
+//        Scanner in = new Scanner(System.in);
+//        int num1, num2, sum;
+//        System.out.print("Enter value of num1 : ");
+//        num1 = in.nextInt();
+//        System.out.print("Enter value of num2 : ");
+//        num2 = in.nextInt();
+//        sum = num1 + num2;
+//        System.out.print("The sum of "+num1+" and "+num2+" is : "+sum);
+//    }
+    public static void main(String[] args) {
+//        sum();
+//        System.out.println("\n");
+//        sum();
+        int ans = sum2();
+        System.out.println("Answer is : "+ans);
+
+
+    }
+}
