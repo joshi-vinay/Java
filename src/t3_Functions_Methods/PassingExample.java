b5a46222b0ae4026b4bfdb6dc6c6ec6a6d0b30b1
Vinay Joshi <joshivinay085@gmail.com>
2024-03-21 23:42:33 +0530

    Passing values

---
+++ src/t3_Functions_Methods/PassingExample.java
@@ -0,0 +1,12 @@
+package t3_Functions_Methods;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String chacha = "Rahul Rana";
+        greet(chacha);
+    }
+
+    private static void greet(String naam) {
+        System.out.println(naam);
+    }
+}
