7af7e91d7a3ddf1ea11196f2fbe959ee0cff4223
Vinay Joshi <joshivinay085@gmail.com>
2024-03-23 12:05:01 +0530

    Assignment Questions

--- src/t3_Functions_Methods/Assignment/Q10.java
+++ src/t3_Functions_Methods/Assignment/Q10.java
@@ -1,15 +1,19 @@
 package t3_Functions_Methods.Assignment;
 
+import java.util.Scanner;
+
 public class Q10 {
     public static void main(String[] args) {
         //  Write a function that returns the sum of first n natural numbers.
-        System.out.print("The sum of first 10 natural numbers is: ");
-        int ans =Natural();
-        System.out.print(ans);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+        int ans =Natural(n);
+        System.out.print("The sum of first "+n+" natural numbers is: "+ans);
     }
-    static int Natural(){
+    static int Natural(int n){
         int sum = 0;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= n; i++) {
             sum +=i;
         }
         return sum;
