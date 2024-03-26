ba3a60a810fee6dd0f17c6c4d240e14605016f8e
Vinay Joshi <joshivinay085@gmail.com>
2024-03-26 15:46:08 +0530

    Finding even digits numbers inside an array

--- src/t5_LinearSearch/EvenDigits.java
+++ src/t5_LinearSearch/EvenDigits.java
@@ -5,8 +5,9 @@ https://leetcode.com/problems/find-numbers-with-even-number-of-digits/descriptio
  */
 public class EvenDigits {
     public static void main(String[] args) {
-        int[] nums={12,345,2,6,7896};
+        int[] nums={12,345,2,6,-7896};
         System.out.print(findNumbers(nums));
+//        System.out.println(digits(0)); // to count the no of digits in a number.
     }
 
     static int findNumbers(int[] nums) {
@@ -18,20 +19,35 @@ public class EvenDigits {
         }
         return count;
     }
+    //  function to check whether a number contains even number of digits or not:-
     static boolean even(int num){
-        int numberOfDigits = digits(num);
+        int numberOfDigits = digits2(num);
         if(numberOfDigits % 2 == 0){
             return true;
         }
         return false;
     }
 
-    static int digits(int num) {
-        int count = 0;
-        while(num>0){
-            num /= 10;
-            count++;
+    //  count number of digits in a number:-
+//    static int digits(int num) {
+//        if(num<0){
+////            num = num*-1;
+//            num *= -1;
+//        }
+//        if(num == 0){
+//            return 1;
+//        }
+//        int count = 0;
+//        while(num>0){
+//            num /= 10;
+//            count++;
+//        }
+//        return count;
+//    }
+    static int digits2(int num){
+        if(num<0){
+            num *= -1;
         }
-        return count;
+        return (int)(Math.log10(num)) + 1;
     }
 }
