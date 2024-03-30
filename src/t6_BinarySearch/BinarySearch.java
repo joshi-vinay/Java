70749e37f233fe393dce7fdd7773f356c4f5dd8f
Vinay Joshi <joshivinay085@gmail.com>
2024-03-30 15:28:37 +0530

    Binary Search Algorithm

--- src/t6_BinarySearch/BinarySearch.java
+++ src/t6_BinarySearch/BinarySearch.java
@@ -2,8 +2,8 @@ package t6_BinarySearch;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,9,11,12,14,20,36,48};
-        int target = 36;
+        int[] arr = {-21,-18,4,6,9,11,12,14,20,36,48};  //  Sorted Array
+        int target = -18;
         int ans = binarySearch(arr,target);
         System.out.println(ans);
 
