370658e1682a5aff8cc5bc348d26dbd1c5258535
Vinay Joshi <joshivinay085@gmail.com>
2025-01-17 21:25:24 +0530

    Cyclic Sort Algorithm

--- src/t7_Sorting/cyclicSort.java
+++ src/t7_Sorting/cyclicSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class cyclicSort {
     public static void main(String[] args) {
         int[] arr = {3, 5, 2, 1, 4};
+//        int[] arr = {3, 0, 2, 1};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -13,12 +14,17 @@ public class cyclicSort {
         int i = 0;
             while(i<arr.length){
 
-            int value = arr[i];
-            //  index = value - 1;
-            int index = value - 1;
-            if(i != index) {
-                swap(arr, i, index);
-            }
+                //  for 0 to n:
+//                int value = arr[i];
+//                if(i != value) {
+//                swap(arr, i, value);
+//            }
+                //  for 1 to n:
+                int value = arr[i];
+                int index = value - 1;
+                if(i != index){
+                    swap(arr, i, index);
+                }
             else{
                 i++;
             }
